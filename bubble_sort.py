7d17bfce552459a8a6d926f1d9531a21bc1d17e5
Sid <44680199+jainsiddharth99@users.noreply.github.com>
2020-12-24 15:06:28 +0530

    Add files via upload

---
+++ bubble_sort.py
@@ -0,0 +1,20 @@
+def bubble_sort(elements):
+    size = len(elements)
+
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+            if elements[j] > elements[j+1]:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+
+        if not swapped:
+            break
+
+
+
+elements = [5,9,2,1,67,34,88,34]
+bubble_sort(elements)
+print(elements)
